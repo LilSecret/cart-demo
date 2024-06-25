03113cc40777243b527c1847cfa618e4de0f6318
Charles <chuckm248@gmail.com>
2024-06-25 13:02:27 -0600

    add functionality to cart item add and minus buttons

--- index.js
+++ index.js
@@ -12,16 +12,40 @@ const addGlobalEventListener = (type, selector, callback) => {
   });
 };
 
+const addOrSubtractCartItem = (title, add) => {
+  for (const item of cart) {
+    if (item["title"] === title) {
+      if (add === "add") {
+        item.quantityAmount += 1;
+      } else {
+        item.quantityAmount -= 1;
+      }
+      return true;
+    }
+  }
+  console.log("Item was not found in cart.");
+  return false;
+};
+
+const removeItemFromCart = (title) => {
+  const itemInUserCart = userCart.querySelector(`[data-item="${title}"]`);
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].title === title) {
+      cart.splice(i, 1);
+    }
+  }
+
+  userCart.removeChild(itemInUserCart);
+};
+
 const addItemToCart = (title, price) => {
   const numberPrice = Number(price.slice(1, price.length));
 
   total += numberPrice;
 
-  for (const item of cart) {
-    if (item["title"] === title) {
-      item.quantityAmount += 1;
-      return;
-    }
+  if (addOrSubtractCartItem(title, "add")) {
+    return;
   }
 
   cart.push({
@@ -41,21 +65,20 @@ const addItemToCart = (title, price) => {
     <h4 class="item-title heading">${title}</h4>
     <div class="flex-group item-amount">
       <div class="">Qty:</div>
-      <div class="">1</div>
+      <div class="quantity-value">1</div>
     </div>
-    <div>${price}</div>
+    <div class="quantity-price-total">${price}</div>
     <div class="flex-group">
-      <button class="btn cart-btn">
+      <button class="btn cart-btn cart-item-add">
         <i class="fa-solid fa-plus"></i>
       </button>
-      <button class="btn cart-btn">
+      <button class="btn cart-btn cart-item-subtract">
         <i class="fa-solid fa-minus"></i>
       </button>
     </div>
     `;
 
   userCart.appendChild(newCartItem);
-  return;
 };
 
 const updateUserTotal = () => {
@@ -64,11 +87,37 @@ const updateUserTotal = () => {
 
 const updateUserCartItems = (title) => {
   const cartItem = userCart.querySelector(`[data-item="${title}"]`);
-  const itemAmount = cartItem.querySelector();
+  const userCartItemQuantity = cartItem.querySelector(".quantity-value");
+  const userQuantityPriceTotal = cartItem.querySelector(
+    ".quantity-price-total"
+  );
 
-  console.log(cartItem);
+  userCartItemQuantity.innerHTML = `${getCartItemPropertyValue(
+    title,
+    "quantityAmount"
+  )}`;
+
+  userQuantityPriceTotal.innerHTML = `$${getCartItemPropertyValue(
+    title,
+    "quantityTotalPrice"
+  )}`;
+
+  if (userCartItemQuantity.innerHTML == 0) {
+    removeItemFromCart(title);
+  }
 };
 
+const getCartItemPropertyValue = (title, property) => {
+  for (const item of cart) {
+    if (item["title"] === title) {
+      return typeof item[property] === "function"
+        ? item[property]()
+        : item[property];
+    }
+  }
+};
+
+// add to cart buttons listener
 addGlobalEventListener("click", ".add-in-cart", (e) => {
   const parent = e.target.parentElement;
   const title = parent.querySelector(".card-title").innerHTML;
@@ -76,6 +125,31 @@ addGlobalEventListener("click", ".add-in-cart", (e) => {
 
   addItemToCart(title, price);
   updateUserTotal();
-  // console.log(cart);
   updateUserCartItems(title);
+});
+
+// add item in cart btn
+addGlobalEventListener("click", ".cart-item-add", (e) => {
+  const cartItem = e.target.parentElement.parentElement;
+  const itemTitle = cartItem.getAttribute("data-item");
+  const itemPrice = getCartItemPropertyValue(itemTitle, "price");
+
+  total += itemPrice;
+
+  addOrSubtractCartItem(itemTitle, "add");
+  updateUserTotal();
+  updateUserCartItems(itemTitle);
+});
+
+// subtract item in cart btn
+addGlobalEventListener("click", ".cart-item-subtract", (e) => {
+  const cartItem = e.target.parentElement.parentElement;
+  const itemTitle = cartItem.getAttribute("data-item");
+  const itemPrice = getCartItemPropertyValue(itemTitle, "price");
+
+  total -= itemPrice;
+
+  addOrSubtractCartItem(itemTitle, "subtract");
+  updateUserTotal();
+  updateUserCartItems(itemTitle);
 });
